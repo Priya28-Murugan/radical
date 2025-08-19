5ed3cc9da5320a5f7a2713593319645d3bd71606
Priya <priya@MacBookAir.lan>
2025-08-19 11:40:09 +0530

    next config file updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // 👈 needed for GitHub Pages
+  },
 };
 
 export default nextConfig;
